e363bb4958741c58b2ef25b0c3949fcf65c5e277
Yassine Louadi <louadiyassine@gmail.com>
2025-06-23 11:11:53 +0300

    Initial commit - Shams Market

---
+++ Documents/shams-web-starter/shams-web/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>مرحبا بك في شمس ماركت 🌞</h1>
+      <p>سوق إلكتروني جزائري شامل</p>
+    </div>
+  );
+}
+
+export default App;
